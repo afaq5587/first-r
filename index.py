5a66d0403744f26f0c7c94b4db1c711058c6b88e
afaq5587 <afaqlatif082@gmail.com>
2024-08-01 13:23:31 +0500

    my revision

---
+++ index.py
@@ -0,0 +1,148 @@
+# a = int ("67")
+# b = 87
+# print(type(b))
+# print(a+b)
+
+# name = input("Enter your name;")
+# print("Welcome" , name)
+# name = input("enter name")
+# age = input("enter your age")
+# study = input("enter your study")
+
+# print("name =" , name)
+# print("age" , age)
+# print("study" , study)
+# name = input("enter name;  ")
+# age = input("enter your age;  ")
+# study = input("enter your study;  ")
+
+# print("name =", name)
+# print("age", age)
+# print("study", study)
+
+# num1 = int(input("enter two numbers; "))
+# num = int(input("enter second number; "))
+# print("sum =" ,num1 + num)
+
+# side = int(input("enter the square side;  "))
+# print("square =" , side ** 2)
+
+# a = float(input("enter a number; "))
+# b = float(input("enter second number; "))
+# print ("avg = ", a+b/2)
+
+# IF AND ELIF AND ELSE STATEMENTS
+
+# play = str(input("lets play game choose number which is not greater than first number; "))
+# a = int(input("enter a number; "))
+# b = int(input("enter second number; "))
+# print(a >= b)
+# if(a >= b):
+# print("well done")  SPACE after if and before print called indentation
+# elif(a<=b):
+#     print("try again")
+
+
+# a =str ("afaq")
+# b = str("latif")
+# final = (a +" " + b)
+# print(final)
+# print(len(final))
+
+
+# indexing
+# name = str("afaq latif")
+# a = name[4] , name[6], name[3]
+# print(a)
+
+# slicing
+# name = str("afaq latif")
+# c = name[2 :5]
+# b = name[:6]
+# a = name[1 : len(name)]
+# print(a)
+# print(b)
+
+# STRING FUCTIONS
+# str.endswith()
+# name = str("my name is afaq")
+# print(name.endswith("aq"))
+# print(name.endswith("na"))
+
+# name = str("my name is afaq")
+# print(name.capitalize())
+
+# name = str("my name is afaq")
+# print(name.replace("a" , 'b'))
+
+# name = str("my name is afaq")
+# print(name.find("m"))
+
+# name = str("my name is afaq")
+# print(name.count("a"))
+
+# practice
+# name = str(input("enter your first name;  "))
+# print(name)
+# print(len(name))
+
+# LISTS
+# marks = [23, 44, 45, 5, 2, 5,]
+# print(marks)
+# print(len(marks))
+# print(type(marks))
+# print(marks[4])
+
+# SLICING
+# marks = [23, 44, 45, 5, 2, 5,]
+# new = marks[1 : 4]
+# print(new)
+
+# marks = [23, 44, 45, 5, 2, 5,]
+# result = marks[  : 5]
+# print(result)
+
+# LIST METHODS
+# APPEND METHOD
+# marks = [23, 44, 45, 5, 2, 5,]
+# marks.append(5)
+# print (marks)
+# sorting    change in to assending order
+# marks = [23, 44, 45, 5, 2, 5,]
+# marks.sort()
+# print(marks)
+
+# desending sorting
+# marks = [23, 44, 45, 5, 2, 5,]
+# marks.sort(reverse = True)
+# print(marks)
+
+# reverse
+# marks = [23, 44, 45, 5, 2, 5,]
+# marks.reverse()
+# print(marks)
+
+# insert
+# marks = [23, 44, 45, 5, 2, 5,]
+# marks.insert(1 , 6)
+# print(marks)
+# marks = [23, 44, 45, 5, 2, 5,]
+# marks.insert(4 , 76)
+# print(marks)
+
+# remove
+# marks = [23, 44, 45, 5, 2, 5,]
+# marks.remove(44)
+# print(marks)
+
+# pop
+# marks = [23, 44, 45, 5, 2, 5,]
+# marks.pop(3)
+# print(marks)
+
+# TUPLES
+# tup =(2 ,4, 6, 7 , 8 , 4)
+# print(tup.index(4))
+# print(tup.count(4))
+
+print("Hello")
